11336d569f4296f4cb79e561bdf92ec3039d32db
Sebastian Gregoricchio <52447016+sebastian-gregoricchio@users.noreply.github.com>
2022-03-07 23:54:10 +0100

    Add files via upload

--- R/qPCR.rna.exp.R
+++ R/qPCR.rna.exp.R
@@ -6,7 +6,7 @@
 #' @param housekeeping.genes String vector with the list of genes that have to be used as target genes. By default \code{NULL}: an error message is printed.
 #' @param max.delta.reps Numeric value indicating the maximum difference among replicate Ct. Default value: \code{0.5}.
 #' @param reference.sample Single string indicating the name of the sample to use as reference for the computation of the FoldChanges. By default \code{NULL}: the first sample in the order is used as reference.
-#' @param exlude.houskeeping.FC Logic value to indicate whether the housekeeping genes should be excluded in the FoldChanges plots. By default \code{TRUE}.
+#' @param exclude.housekeeping.FC Logic value to indicate whether the housekeeping genes should be excluded in the FoldChanges plots. By default \code{TRUE}.
 #' @param exclude.samples String vector indicating the samples that should be exuded in the expression and FoldChange plots. By default \code{NULL}.
 #' @param fix.y.axis Logic value indicating whether the y-axis of the plots should be kept fixed among all the genes. By default \code{FALSE}.
 #' @param x.labels.rotation Numeric value indicating the degrees of x-axis's labels rotation. By default \code{45}.
@@ -31,14 +31,14 @@
 #'
 #'
 #' @export qPCR.rna.exp
- 
+
 
 
 qPCR.rna.exp = function(results.file,
                         housekeeping.genes = NULL,
                         max.delta.reps = 0.5,
                         reference.sample = NULL,
-                        exlude.houskeeping.FC = TRUE,
+                        exclude.housekeeping.FC = TRUE,
                         exclude.samples = NULL,
                         fix.y.axis = FALSE,
                         x.labels.rotation = 45,
@@ -49,16 +49,16 @@ qPCR.rna.exp = function(results.file,
                         file.tail = TRUE,
                         samples.order = NULL,
                         ignore.reps.errors = FALSE) {
-  
+
   #-----------------------------#
   # Check if Rseb is up-to-date #
   Rseb::actualize(update = F, verbose = F)   #
   #-----------------------------#
-  
+
   # Libraries
   require(dplyr)
   require(ggplot2)
-  
+
   # Reading the file
   if ("character" %in% class(results.file)) {
     results =
@@ -66,16 +66,16 @@ qPCR.rna.exp = function(results.file,
                          sheet = results.sheet.position,
                          skip = rows.to.skip,
                          col_names = file.header)
-    
+
     if (file.tail == TRUE) {# Removing last 5 rows
       results = results[-((nrow(results)-4):nrow(results)),]
     }
   } else {
     results = results.file
   }
-  
-  
-  
+
+
+
   # Check reference sample
   if (!is.null(reference.sample) | (length(reference.sample) > 1)) {
     if (!(reference.sample %in% results$`Sample Name`)) {
@@ -83,9 +83,9 @@ qPCR.rna.exp = function(results.file,
                             paste(unique(results$`Sample Name`), collapse = ", "))))
     }
   } else {reference.sample = results$`Sample Name`[[1]]}
-  
-  
-  
+
+
+
   # Check excluded samples
   if (!is.null(exclude.samples)) {
     if (F %in% (exclude.samples %in% results$`Sample Name`)) {
@@ -93,22 +93,22 @@ qPCR.rna.exp = function(results.file,
                      paste(unique(results$`Sample Name`), collapse = ", "), ". \nHowever, the function will run anyway."))
     }
   }
-  
-  
-  
+
+
+
   # Define the sample order
   actual_order = unique(results$`Sample Name`)
-  
+
   if (is.null(samples.order)) {
     samples.order = c(reference.sample, actual_order[actual_order != reference.sample])
   } else {samples.order = samples.order}
-  
+
   results =
     dplyr::mutate(.data = results, `Sample Name` = factor(`Sample Name`, levels = samples.order)) %>%
     dplyr::arrange(`Sample Name`, `Target Name`)
-  
-  
-  
+
+
+
   # Set 'undetermined' samples to 0
   results =
     results %>%
@@ -117,56 +117,56 @@ qPCR.rna.exp = function(results.file,
                             x = results$CT,
                             ignore.case = T)) %>%
     dplyr::mutate(CT = as.numeric(CT))
-  
-  
+
+
   # Check housekeeping genes
   if (F %in% (housekeeping.genes %in% results$`Target Name`) | is.null(housekeeping.genes)) {
     return(warning(paste0("The 'housekeeping.genes' must be among the targets present in your file -> ",
                           paste(unique(results$`Target Name`), collapse = ", "))))
   }
-  
-  
+
+
   # Re-shape table for replicates
   reps_tb = data.frame()
-  
+
   for (s in levels(results$`Sample Name`)) {
     for (t in unique(results$`Target Name`)) {
       current_tb = dplyr::filter(.data = results,
                                  `Sample Name` == s,
                                  `Target Name` == t)
-      
+
       current_reps_tb = data.frame(t(data.frame(c(s, t, current_tb$CT))))
       colnames(current_reps_tb) = c("Sample Name", "Target Name", paste0("rep", 1:(ncol(current_reps_tb)-2)))
-      
+
       reps_tb = plyr::rbind.fill(reps_tb, current_reps_tb)
     }
   }
-  
+
   rownames(reps_tb) = NULL
   reps_tb[is.na(reps_tb)] = 0
-  
-  
+
+
   # Compute reps differences
   combinations = combn(colnames(reps_tb[-c(1:2)]), 2)
-  
+
   for (c in 1:ncol(combinations)) {
     reps_tb =
       reps_tb %>%
       dplyr::mutate(diff = abs(as.numeric(reps_tb[,combinations[1,c]]) - as.numeric(reps_tb[,combinations[2,c]])))
-    
+
     colnames(reps_tb)[ncol(reps_tb)] = paste0(combinations[1,c], "-", combinations[2,c])
   }
-  
-  
+
+
   # Remove too different CTs
   reps_tb_clean = reps_tb
   rep_names = unique(c(combinations[1, ], combinations[2, ]))
   to_keep_tb = reps_tb[(ncol(reps_tb) - ncol(combinations) + 1):ncol(reps_tb)] < max.delta.reps
-  
+
   list_to_remove = list()
   for (i in 1:nrow(to_keep_tb)) {
     failed = sum(to_keep_tb[i, ] == F)
-    
+
     if (failed != 0) {
       if ((failed == (ncol(combinations) - 2)) | (failed == ncol(combinations))) {
         list_to_remove[[i]] = c("all")
@@ -181,9 +181,9 @@ qPCR.rna.exp = function(results.file,
       if (failed == 0) {list_to_remove[[i]] = c("none")}
     }
   }
-  
-  
-  
+
+
+
   # Substitute the reps-to-remove with NA
   for (i in 1:length(list_to_remove)) {
     if (list_to_remove[[i]][1] != "none") {
@@ -199,12 +199,12 @@ qPCR.rna.exp = function(results.file,
       }
     }
   }
-  
-  
-  
+
+
+
   # Plot the reps difference
   differences_table = data.frame()
-  
+
   for (r in paste0(combinations[1,], "-", combinations[2,])) {
     differences_table = rbind(differences_table,
                               data.frame(Sample_Name = reps_tb$`Sample Name`,
@@ -212,13 +212,13 @@ qPCR.rna.exp = function(results.file,
                                          delta = reps_tb[,r],
                                          comparison = r))
   }
-  
+
   differences_table =
     differences_table %>%
     dplyr::mutate(Sample_Name = factor(Sample_Name, levels = rev(levels(results$`Sample Name`))),
                   good = factor(delta < max.delta.reps, levels = c(T, F)))
-  
-  
+
+
   reps_difference_plot =
     ggplot(differences_table,
            aes(y = Sample_Name,
@@ -233,74 +233,74 @@ qPCR.rna.exp = function(results.file,
     theme(axis.text = element_text(color = "#000000"),
           axis.text.x = element_text(angle = x.labels.rotation,
                                      hjust = 1))
-  
-  
-  
+
+
+
   # Decide which table to use
   if (ignore.reps.errors == T) {analyses_tb = reps_tb} else {analyses_tb = reps_tb_clean}
-  
+
   # Convert reps values to numeric
   for (i in rep_names) {analyses_tb[,i] = as.numeric(analyses_tb[,i])}
-  
+
   # Compute means of replicates
   analyses_tb = dplyr::mutate(.data = analyses_tb,
                               CT_reps_mean = rowMeans(as.matrix(analyses_tb[,rep_names]), na.rm = T))
-  
-  
-  
-  
+
+
+
+
   ##########################
   # Compute the 2^-(target-housekeeping) and FoldChange
   list_norm_tb = list()
-  
+
   for (h in housekeeping.genes) {
     current_normalized_analyses_tb = data.frame()
-    
+
     for (t in unique(results$`Target Name`)) {
       # Subset the analyses table
       current_gene = analyses_tb %>% dplyr::filter(`Target Name` == t)
-      
+
       current_house =
         analyses_tb %>%
         dplyr::filter(`Target Name` == h) %>%
         dplyr::mutate(house_mean = CT_reps_mean) %>%
         dplyr::select(`Sample Name`, house_mean)
-      
+
       current_norm =
         dplyr::left_join(current_gene, current_house, by = "Sample Name") %>%
         dplyr::mutate(norm_exp = 2^-(CT_reps_mean-house_mean))
-      
+
       current_norm =
         current_norm %>%
         dplyr::mutate(FoldChange = norm_exp / current_norm[which(current_norm$`Sample Name` == reference.sample),"norm_exp"]) %>%
         dplyr::select(-house_mean)
-      
+
       colnames(current_norm)[(ncol(current_norm)-1):ncol(current_norm)] = c(paste0("exp_norm_to_",h),
                                                                             paste0("FC_over_", reference.sample))
       current_normalized_analyses_tb = rbind(current_normalized_analyses_tb, current_norm)
     }
-    
+
     list_norm_tb[[which(housekeeping.genes == h)]] = current_normalized_analyses_tb
     names(list_norm_tb)[which(housekeeping.genes == h)] = h
   }
-  
-  
-  
-  
+
+
+
+
   #############################
   # Generate the plots
   exp_plots = list()
   FC_plots = list()
-  
+
   for (h in housekeeping.genes) {
     tb =
       list_norm_tb[[h]] %>%
       dplyr::mutate(`Sample Name` = factor(`Sample Name`, levels = levels(results$`Sample Name`))) %>%
       dplyr::filter(!(`Sample Name` %in% exclude.samples))
-    
+
     colnames(tb)[grepl("exp_norm_to_|FC_over_", colnames(tb))] = c("exp", "FC")
-    
-    
+
+
     exp_plots[[which(housekeeping.genes == h)]] =
       ggplot(data = tb %>% dplyr::filter(`Target Name` != h),
              aes(x = `Sample Name`,
@@ -317,12 +317,12 @@ qPCR.rna.exp = function(results.file,
       theme(axis.text = element_text(color = "#000000"),
             axis.text.x = element_text(angle = x.labels.rotation,
                                        hjust = 1))
-    
-    
+
+
     # Remove housekeeping.genes from FC plots (if required)
-    if (exlude.houskeeping.FC == T) {tb = dplyr::filter(.data = tb, !(`Target Name` %in% housekeeping.genes))}
-    
-    
+    if (exclude.housekeeping.FC == T) {tb = dplyr::filter(.data = tb, !(`Target Name` %in% housekeeping.genes))}
+
+
     FC_plots[[which(housekeeping.genes == h)]] =
       ggplot(data = tb,
              aes(x = `Sample Name`,
@@ -340,50 +340,50 @@ qPCR.rna.exp = function(results.file,
       theme(axis.text = element_text(color = "#000000"),
             axis.text.x = element_text(angle = x.labels.rotation,
                                        hjust = 1))
-    
-    
+
+
     names(exp_plots)[which(housekeeping.genes == h)] = h
     names(FC_plots)[which(housekeeping.genes == h)] = h
   }
-  
-  
-  
-  
+
+
+
+
   ###############
   # COMPUTE THE MEANS BETWEEN HOUSEKEEPING
-  
+
   FC_matrix = list()
-  
+
   for (h in 1:length(housekeeping.genes)) {
     FC_matrix[[h]] = list_norm_tb[[h]][which(grepl("FC_over", colnames(list_norm_tb[[h]])))]
   }
-  
+
   FC_matrix =
     data.frame(FC_matrix) %>%
     dplyr::mutate(mean_FC_over_refereceSample = rowMeans(as.matrix(data.frame(FC_matrix)), na.rm = T),
                   mean_FC_SEM = matrixStats::rowSds(as.matrix(data.frame(FC_matrix)), na.rm = T) / sqrt(length(housekeeping.genes)))
-  
+
   mean_table =
     list_norm_tb[[1]][,-((ncol(list_norm_tb[[1]])-3):ncol(list_norm_tb[[1]]))] %>%
     mutate(mean_FC_over_refereceSample = FC_matrix$ mean_FC_over_refereceSample,
            mean_FC_SEM = FC_matrix$mean_FC_SEM)
-  
+
   names(mean_table)[which(colnames(mean_table) == "mean_FC_over_refereceSample")] = paste0("mean_FC_over_", reference.sample)
-  
+
   list_norm_tb[[length(list_norm_tb)+1]] = mean_table
   names(list_norm_tb)[length(list_norm_tb)] = "mean_FC_housekeeping"
-  
-  
+
+
   # generate mean plots
   tb =
     list_norm_tb$mean_FC_housekeeping %>%
     dplyr::mutate(`Sample Name` = factor(`Sample Name`, levels = samples.order)) %>%
     dplyr::filter(!(`Sample Name` %in% exclude.samples),
                   !(`Target Name` %in% housekeeping.genes))
-  
+
   colnames(tb)[grepl("FC_over_", colnames(tb))] = "FC"
-  
-  
+
+
   FC_plots[[length(FC_plots)+1]] =
     ggplot(data = tb,
            aes(x = `Sample Name`,
@@ -410,14 +410,14 @@ qPCR.rna.exp = function(results.file,
     theme(axis.text = element_text(color = "#000000"),
           axis.text.x = element_text(angle = x.labels.rotation,
                                      hjust = 1))
-  
+
   names(FC_plots)[length(FC_plots)] = "mean.FC.housekeeping"
-  
-  
-  
+
+
+
   #########################
   # Export the output
-  
+
   return(list(original.table = results,
               reshaped.table = reps_tb,
               reshaped.table.cleaned = reps_tb_clean,
@@ -425,5 +425,5 @@ qPCR.rna.exp = function(results.file,
               analyzed.data = list_norm_tb,
               expression.plots = exp_plots,
               foldChange.plots = FC_plots))
-  
+
 } # END function
--- R/qPCR.rna.mean.reps.R
+++ R/qPCR.rna.mean.reps.R
@@ -5,7 +5,7 @@
 #' @param reps.list A list of \link{qPCR.rna.exp} results (and/or 'mean_FC_housekeeping' data.frames).
 #' @param housekeeping.genes String vector with the list of genes that have to be used as target genes. By default \code{NULL}: if the input is a list of qPCR.rna.exp objects, the housekeeping genes are retrieved automatically.
 #' @param exclude.samples String vector indicating the samples that should be exuded in the expression and FoldChange plots. By default \code{NULL}.
-#' @param exlude.houskeeping.genes Logic value to indicate whether the housekeeping genes should be excluded in the plot. By default \code{TRUE}.
+#' @param exclude.housekeeping.genes Logic value to indicate whether the housekeeping genes should be excluded in the plot. By default \code{TRUE}.
 #' @param plot.color Single string to indicate the color to use for the bar plot. Default value: \code{#D1718B}.
 #' @param fix.y.axis Logic value indicating whether the y-axis of the plots should be kept fixed among all the genes. By default \code{FALSE}.
 #' @param text.size Numeric value to indicate the size of the text for the number above the bars. Default \code{3}.
@@ -34,29 +34,29 @@ qPCR.rna.mean.reps = function(
     text.size = 3,
     x.labels.rotation = 45,
     force = F) {
-  
-  
+
+
   #-----------------------------#
   # Check if Rseb is up-to-date #
   Rseb::actualize(update = F, verbose = F)   #
   #-----------------------------#
-  
+
   # libraries
   require(dplyr)
   require(ggplot2)
-  
-  
+
+
   # Check plot color
   if ((plot.color != "#d1718b") & !Rseb::is.color(plot.color)) {
     message("The plot.color value is not recognized. Coerced to '#d1718b' by default.")
     plot.color = "#d1718b"
   }
-  
-  
+
+
   # Select columns in the list
   check_names = c()
   housekeeping.genes.list = c()
-  
+
   for (i in 1:length(reps.list)) {
     if ("list" %in% class(reps.list[[i]])) {
       if (is.null(housekeeping.genes)) {housekeeping.genes.list = c(housekeeping.genes.list, names(reps.list[[i]]$expression.plots))}
@@ -65,10 +65,10 @@ qPCR.rna.mean.reps = function(
     reps.list[[i]] = reps.list[[i]][, c(1,2,(ncol(reps.list[[i]])-1))]
     check_names = c(check_names, colnames(reps.list[[i]][3]))
   }
-  
+
   housekeeping.genes.list = unique(housekeeping.genes.list)
-  
-  
+
+
   # Check excluded samples
   if (!is.null(exclude.samples)) {
     if (F %in% (exclude.samples %in% reps.list[[i]]$`Sample Name`)) {
@@ -76,25 +76,25 @@ qPCR.rna.mean.reps = function(
                      paste(unique(reps.list[[i]]$`Sample Name`), collapse = ", "), ". \nHowever, the function will run anyway."))
     }
   }
-  
-  
+
+
   # Define order
   samples_order = unique(reps.list[[i]]$`Sample Name`)
-  
-  
+
+
   # Check that the FC are coherent
   if ((length(unique(check_names)) > 1) & (force == F)) {
     return(warning("The FoldChanges provided correspond to comparisons made on different reference samples.\nIf you wish to perform the analyses anyway, set the parameter 'force = TRUE'."))
   }
-  
-  
+
+
   # Combine tables
   combined_table = do.call("rbind", reps.list)
   comparison_name = colnames(combined_table)[3]
   colnames(combined_table)[3] = "FoldChange"
-  
+
   # Calculate the statistics
-  stat_table = 
+  stat_table =
     combined_table %>%
     dplyr::group_by(`Sample Name`, `Target Name`) %>%
     dplyr::summarise(n = n(),
@@ -103,24 +103,24 @@ qPCR.rna.mean.reps = function(
                      .groups = "drop_last") %>%
     dplyr::mutate(SEM = SD / sqrt(n)) %>%
     dplyr::filter(!(`Sample Name` %in% exclude.samples))
-  
+
   stat_table =
     stat_table %>%
     dplyr::mutate(`Sample Name` = factor(`Sample Name`, samples_order)) %>%
     dplyr::arrange(`Target Name`, `Sample Name`)
-  
-  
+
+
   # Filter housekeeping if required
   if (exclude.housekeeping.genes == TRUE) {
     table = stat_table %>% dplyr::filter(!(`Target Name` %in% housekeeping.genes.list))
   } else (table = stat_table)
-  
+
   table =
     dplyr::mutate(.data = table, `Sample Name` = factor(`Sample Name`, samples_order)) %>%
     dplyr::arrange(`Target Name`, `Sample Name`) %>%
     dplyr::filter(!(`Sample Name` %in% exclude.samples))
-  
-  
+
+
   # Generate plot
   mean_FC_plot =
     ggplot(data = table,
@@ -161,13 +161,13 @@ qPCR.rna.mean.reps = function(
     theme(axis.text = element_text(color = "#000000"),
           axis.text.x = element_text(angle = x.labels.rotation,
                                      hjust = 1))
-  
-  
-  
+
+
+
   # Return output
   return(list(mean.reps.table = stat_table,
               mean.reps.FC.plot = mean_FC_plot))
-  
+
 } # END function
 
 
