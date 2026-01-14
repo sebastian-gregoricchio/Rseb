a0ae52de19eafce5ea0aed2ba548bdf1797acf4a
Sebastian Gregoricchio <sebastian.gregoricchio@gmail.com>
2026-01-14 11:08:51 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach =
   function(libname, pkgname){
     msg = paste0("Loading required package: ", pkgname,"\n\n",
-                 "If you are using DEprot in your work please cite:\n\n",
+                 "If you are using ", pkgname, " in your work please cite:\n\n",
                  "HDAC1 and PRC2 mediate combinatorial control in SPI1/PU.1-dependent gene repression in murine erythroleukaemia.\n",
                  "Gregoricchio S. et al., Nucleic Acid Research (2022)\n",
                  "doi: 10.1093/nar/gkac613")
